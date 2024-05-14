2878aa3b40b5acb1fd355976aa0103fdfafc352f
eofitg <eofitg@outlook.com>
2024-05-14 20:52:03 +0800

    Fix grayscaling.py to allow it to handle PNG images with alpha channels.

--- utils/grayscaling.py
+++ utils/grayscaling.py
@@ -1,14 +1,22 @@
-import cv2
+import os
+from PIL import Image
 
-input_path = 'input/'
-
-
-def gs(path):
-    im = cv2.imread(input_path + path)
-    gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-    return gray
+input_path = './input/'
+output_path = './output/'
 
 
-def img_gs(im):
-    return cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+def build(dir):
+    for item in os.scandir(dir):
+        if item.is_dir():
+            build(item)
+        elif item.is_file() and item.path.endswith('.png'):
+            img = Image.open(item.path).convert('RGBA')
+            r, g, b, a = img.split()
+            grey_img = Image.merge('RGB', (r, g, b)).convert('L')
+            result_img = Image.merge('LA', (grey_img, a))
+
+            path = output_path + item.path[len(input_path):-len(item.name)]
+            if not os.path.exists(path):
+                os.makedirs(path)
+            result_img.save(output_path + item.path[8:])
 
