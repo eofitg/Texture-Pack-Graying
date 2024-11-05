53c7aefa54d4e2f6b2401834104dcca3558ff6a5
eofitg <eofitg@outlook.com>
2024-11-05 08:40:19 +0800

    Add nest_check

--- main.py
+++ main.py
@@ -14,6 +14,37 @@ input_path = './input/'
 output_path = './output/'
 
 
+# if or not this path only has one dir
+# True -> return the path of this dir
+# False -> return ""
+def nest_check(path):
+    count = False
+    res_path = ""
+
+    for item in os.scandir(path):
+
+        # ignore hidden items
+        if item.name.startswith('.'):
+            continue
+
+        # should not have any files
+        if item.is_file():
+            return ""
+
+        # found dir
+        else:
+            if not count:
+                count = True
+                res_path = item.path
+
+            # found another dir while has counted one
+            else:
+                return ""
+
+    # return the only directory's path
+    return res_path
+
+
 if __name__ == '__main__':
 
     # clear output folder
@@ -28,6 +59,14 @@ if __name__ == '__main__':
         pack_path = ot.get_pack_path(pack)
         if building_message:
             print('Building ' + pack_path + ' ......')
+
+        # this pack may be nested like
+        # ./input/XXX/{pack}
+        # for this, recognize dirs with only 1 internal dir as a layer of nesting
+        if nest_check(pack_path) != "":
+            pack_path = nest_check(pack_path)
+            if building_message:
+                print("Building nested dir \"" + pack_path + "\" ......")
 
         for file in os.scandir(pack_path):
             if file.is_file() and not file.name.startswith('.'):
