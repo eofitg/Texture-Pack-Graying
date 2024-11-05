332759959265a0dbb8abf720e67acd885fd74154
eofitg <eofitg@outlook.com>
2024-11-05 11:20:10 +0800

    Cleanup: config_reader

--- config_reader.py
+++ config_reader.py
@@ -1,11 +1,9 @@
 import yaml
 
 
-with open('./config.yml', 'r',encoding="utf-8") as f:
-    config = yaml.safe_load(f)
-
-
 def get(key_list):
+    with open('./config.yml', 'r', encoding="utf-8") as f:
+        config = yaml.safe_load(f)
     value = config
     keys = key_list.split('.')
     for key in keys:
