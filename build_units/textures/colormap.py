44fc1bb9c594a0a214876384a565ca6fb657eeaf
eofitg <eofitg@outlook.com>
2025-02-14 16:35:29 +0800

    **Brightness Bug**: Fixed in `/texture/colormap/`

--- build_units/textures/colormap.py
+++ build_units/textures/colormap.py
@@ -1,25 +1,24 @@
 from build_units.build_texture import BuildTexture
-import utils.operating_tools as ot
+import utils.grayscaling as gs
 import config_reader as cr
 
 
 class Colormap(BuildTexture):
 
     def __init__(self, texture_path):
-        vanilla_path = './resource/1.8.9/assets/minecraft/gray/textures/colormap/'
+        vanilla_path = './resource/1.8.9/assets/minecraft/textures/colormap/'
         super().__init__(vanilla_path, texture_path)
 
     # ./input/{pack}/assets/minecraft/textures/colormap
     def build(self):
 
+        # if colormap part does not need to grayscale
         if not cr.get('texture.colormap'):
             return False
-
-        output_path = ot.turn_output_path(self.resource_path)
 
         checklist = super().build()
         # files this pack didn't modify but need to grayscale
         for item in checklist:
-            ot.copy_file(self.vanilla_path + item, output_path)
+            gs.build_vanilla_file(self.vanilla_path, self.resource_path, item)
 
         return True
