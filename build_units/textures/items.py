cc8b74aee8b80d1b26ac0f90bbf1ff57e5790406
eofitg <eofitg@outlook.com>
2025-02-19 14:34:43 +0800

    **Brightness Bug**: Fixed in `/texture/items/`

--- build_units/textures/items.py
+++ build_units/textures/items.py
@@ -1,25 +1,24 @@
 from build_units.build_texture import BuildTexture
-import utils.operating_tools as ot
+import utils.grayscaling as gs
 import config_reader as cr
 
 
 class Items(BuildTexture):
 
     def __init__(self, texture_path):
-        vanilla_path = './resource/1.8.9/assets/minecraft/gray/textures/items/'
+        vanilla_path = './resource/1.8.9/assets/minecraft/textures/items/'
         super().__init__(vanilla_path, texture_path)
 
     # ./input/{pack}/assets/minecraft/textures/items
     def build(self):
 
+        # if item part does not need to grayscale
         if not cr.get('texture.items'):
             return False
-
-        output_path = ot.turn_output_path(self.resource_path)
 
         checklist = super().build()
         # files this pack didn't modify but need to grayscale
         for item in checklist:
-            ot.copy_file(self.vanilla_path + item, output_path)
+            gs.build_vanilla_file(self.vanilla_path, self.resource_path, item)
 
         return True
