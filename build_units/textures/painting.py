0dac954ae482e6515dbb3b791bf924df3ced0925
eofitg <eofitg@outlook.com>
2025-02-19 14:41:11 +0800

    **Brightness Bug**: Fixed in `/texture/painting/`

--- build_units/textures/painting.py
+++ build_units/textures/painting.py
@@ -1,25 +1,24 @@
 from build_units.build_texture import BuildTexture
-import utils.operating_tools as ot
+import utils.grayscaling as gs
 import config_reader as cr
 
 
 class Painting(BuildTexture):
 
     def __init__(self, texture_path):
-        vanilla_path = './resource/1.8.9/assets/minecraft/gray/textures/painting/'
+        vanilla_path = './resource/1.8.9/assets/minecraft/textures/painting/'
         super().__init__(vanilla_path, texture_path)
 
     # ./input/{pack}/assets/minecraft/textures/painting
     def build(self):
 
+        # if painting part does not need to grayscale
         if not cr.get('texture.painting'):
             return False
-
-        output_path = ot.turn_output_path(self.resource_path)
 
         checklist = super().build()
         # files this pack didn't modify but need to grayscale
         for item in checklist:
-            ot.copy_file(self.vanilla_path + item, output_path)
+            gs.build_vanilla_file(self.vanilla_path, self.resource_path, item)
 
         return True
