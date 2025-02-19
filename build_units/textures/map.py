54000a8ed59f698534dae439400de17481b2c5c5
eofitg <eofitg@outlook.com>
2025-02-19 14:38:48 +0800

    **Brightness Bug**: Fixed in `/texture/map/`

--- build_units/textures/map.py
+++ build_units/textures/map.py
@@ -1,25 +1,24 @@
 from build_units.build_texture import BuildTexture
-import utils.operating_tools as ot
+import utils.grayscaling as gs
 import config_reader as cr
 
 
 class Map(BuildTexture):
 
     def __init__(self, texture_path):
-        vanilla_path = './resource/1.8.9/assets/minecraft/gray/textures/map/'
+        vanilla_path = './resource/1.8.9/assets/minecraft/textures/map/'
         super().__init__(vanilla_path, texture_path)
 
     # ./input/{pack}/assets/minecraft/textures/map
     def build(self):
 
+        # if map part does not need to grayscale
         if not cr.get('texture.map'):
             return False
-
-        output_path = ot.turn_output_path(self.resource_path)
 
         checklist = super().build()
         # files this pack didn't modify but need to grayscale
         for item in checklist:
-            ot.copy_file(self.vanilla_path + item, output_path)
+            gs.build_vanilla_file(self.vanilla_path, self.resource_path, item)
 
         return True
